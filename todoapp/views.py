a1b2e7796bdc5b3b3a2b9a835a08fba181f810ca
Farook <farookmuhammed379@gmail.com>
2022-09-14 15:56:36 +0530

    initail commit

---
+++ todoapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import View
+
+
+
+
+class SignUpView(View):
+    def get(self,request,*args,**kwargs):
+        return render(request,"registration.html")
